e130936ef30986071e8e86dcb3489a9330612804
TheSolid-Kang <tortoise8947@gmail.com>
2023-06-22 19:17:32 +0900

    CList 구현 및 예제

---
+++ Practice/src/CObj/CList.java
@@ -0,0 +1,31 @@
+package CObj;
+
+public class CList<T> {
+    private class CNode {
+        public CNode(){
+
+        }
+        public CNode(Object _element){
+            m_data = _element;
+        }
+        Object m_data;
+        CNode m_nextNode;
+    }
+
+    private CNode m_rootNode;
+
+    public void push_front(T _element){
+        if(m_rootNode == null){
+            m_rootNode = new CNode(_element);
+        }else {
+            CNode curNode = new CNode(_element);
+            curNode.m_nextNode = m_rootNode;
+            m_rootNode = curNode;
+        }
+    }
+    public <T> T pop_front() {
+        CNode curNode = m_rootNode;
+        m_rootNode = m_rootNode.m_nextNode;
+        return (T) curNode.m_data;
+    }
+}
--- Practice/src/IPractice/CPractice_2/CPractice_2.java
+++ Practice/src/IPractice/CPractice_2/CPractice_2.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import CObj.CDinamicArray;
+import CObj.CList;
 import Headers.CIO;
 import IPractice.IPractice;
 
@@ -21,8 +22,8 @@ public class CPractice_2 implements IPractice{
     public void initialize() {
         titles = new ArrayList<>();
         titles.add("\n연습 선택");
-        titles.add("\n1. DinamicArray");
-        titles.add("\n2. ");
+        titles.add("\n1. DinamicArray ");
+        titles.add("\n2. CForwardList ");
         titles.add("\n99. EXIT");
         titles.add("\nChoose: ");
     }
@@ -34,6 +35,7 @@ public class CPractice_2 implements IPractice{
                 practice_1();
                 break;
             case 2:
+                practice_2();
                 break;
             case 99: 
                 return 99;
@@ -46,7 +48,7 @@ public class CPractice_2 implements IPractice{
     @Override
     public void render() {
         for(String title : titles)
-            System.out.println(title);
+            System.out.print(title);
     }
 
     @Override
@@ -54,6 +56,7 @@ public class CPractice_2 implements IPractice{
 
     }
 
+    //CDinamicArray 예제
     private void practice_1() {
         CDinamicArray<Integer> lists = new CDinamicArray<>(256);
         ArrayList<Integer> lii = new ArrayList<>(256); //256 크기의 컨테이너가 생성된다.
@@ -65,6 +68,23 @@ public class CPractice_2 implements IPractice{
         for(int i = 0; i < lists.Size(); ++i) {
             System.out.println(lists.Get(i).toString());
         }
+        //error 띄우기
+        lists.Get(10);
+    }
+    //CForwardList 예제
+    private void practice_2(){
+        CList<Integer> lists = new CList<>();
+        //1. push_front
+        lists.push_front(1);
+        lists.push_front(2);
+        lists.push_front(3);
+        lists.push_front(4);
+
+        //2. pop_front
+        for(int i = 0; i < 4; ++i){
+            System.out.println( lists.pop_front().toString());
+        }
+
     }
 
 
