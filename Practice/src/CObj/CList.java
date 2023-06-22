6862fa657f0a033a8328be46323e6d51311c9222
TheSolid-KANG <tortoise8947@gmail.com>
2023-06-22 22:51:40 +0900

    no message

--- Practice/src/CObj/CList.java
+++ Practice/src/CObj/CList.java
@@ -1,36 +1,7 @@
 package CObj;
 
-<<<<<<< HEAD
 
 public class CList<T> extends CObj {
-    public class CNode<T> {
-        public CNode(T _item){
-            m_curItem = _item;
-        }
-        private T m_curItem;
-        private CNode<T> m_nextNode;
-        private CNode<T> m_prevNode;
-    }
-    public CList(){
-
-    }
-
-    CNode<T> m_rootNode;
-
-    public <T> void Add(T _element) {
-
-    }
-    public <T> T Get() {
-        Object node = m_rootNode;
-        //m_rootNode.m_nextNode;
-        return node.m_curItem;
-    }
-
-
-
-
-=======
-public class CList<T> {
     private class CNode {
         public CNode(){
 
@@ -58,5 +29,4 @@ public class CList<T> {
         m_rootNode = m_rootNode.m_nextNode;
         return (T) curNode.m_data;
     }
->>>>>>> e130936ef30986071e8e86dcb3489a9330612804
 }
